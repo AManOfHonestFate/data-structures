ee73048788a78757de84501e44a8ef9fa289a335
Egor <khromov_dev@mail.ru>
2022-10-25 02:15:53 +0300

    list base

--- dstr/list.go
+++ dstr/list.go
@@ -1,1 +1,14 @@
 package dstr
+
+type node[T any] struct {
+	next, previous *node[T]
+	value          T
+}
+
+type List[T any] struct {
+	head, tail *node[T]
+}
+
+type ListI[T any] interface {
+	NewList() *List[T]
+}
